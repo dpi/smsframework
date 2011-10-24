5d4465e0b8fb1143c8f4147b4d63ce7076633f38
Chris Hood <chris@univate.com.au>
2011-10-24 16:54:32 +1000

    first pass at D7 port for sms_user

--- modules/sms_user/sms_user.js
+++ modules/sms_user/sms_user.js
@@ -1,9 +1,12 @@
-  Drupal.behaviors.smsUser = function(context) {
-    var $smstext = $('#edit-sms-text');
-    var $keystrokes = $('#keystrokes span').eq(0).text('0 / ');
-    $smstext.bind('keyup', function(e) {
-      var chars = $smstext.val().length;
-      $keystrokes.text(chars + ' / ');
-    });
+(function ($) {
+  Drupal.behaviors.smsUser = {
+    attach: function($context) {
+      var $smstext = $('#edit-sms-text');
+      var $keystrokes = $('#keystrokes span').eq(0).text('0 / ');
+      $smstext.bind('keyup', function(e) {
+        var chars = $smstext.val().length;
+        $keystrokes.text(chars + ' / ');
+      });
+    }
   };
-
+}(jQuery));
