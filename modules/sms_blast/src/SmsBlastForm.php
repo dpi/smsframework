8410813af421fabe2ce6b5d2303db33cf405397a
Aniebiet Udoh <almaudoh@yahoo.com>
2014-06-16 00:27:04 +0100

    Issue #2093615 by almaudoh: ported sms_blast module to Drupal 8.

---
+++ modules/sms_blast/src/SmsBlastForm.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\sms_blast\SmsBlastForm
+ */
+
+namespace Drupal\sms_blast;
+
+use Drupal\Core\Form\FormBase;
+
+class SmsBlastForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    return 'sms_blast_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state)
+  {
+    $form['message'] = array(
+      '#type' => 'textarea',
+      '#title' => $this->t('Message'),
+      '#cols' => 60,
+      '#rows' => 5,
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Send'),
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state)
+  {
+    $result = db_select('sms_user', 'su')
+                ->fields('su', array('uid'))
+                ->condition('status', 2)
+                ->execute();
+
+    $count = 0;
+    foreach ($result as $row) {
+      sms_user_send($row->uid, $form_state['values']['message']);
+      $count++;
+    }
+    if ($count) {
+      drupal_set_message($this->t('The message was sent to %count users.', array('%count' => $count)));
+    }
+    else {
+      drupal_set_message($this->t('There are 0 users with confirmed phone numbers. The message was not sent.'));
+    }
+  }
+}
