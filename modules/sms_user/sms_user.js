a2338791df5825e10bc26bf00bc4598f7a2aa8ea
Aniebiet Udoh <almaudoh@yahoo.com>
2014-08-05 23:15:56 +0100

    Issue #2295971 by almaudoh: Fixed documentation on sms_user module.

--- modules/sms_user/sms_user.js
+++ modules/sms_user/sms_user.js
@@ -1,5 +1,11 @@
 (function ($) {
   Drupal.behaviors.smsUser = {
+    /**
+     * Adds javascript behavior to display the number of characters typed in
+     * sms_user actions form.
+     *
+     * @param $context
+     */
     attach: function($context) {
       var $smstext = $('#edit-sms-text');
       var $keystrokes = $('#keystrokes span').eq(0).text('0 / ');
