55c0873dae558016219ff9317862200e0f34df3a
danph <danph@81431.no-reply.drupal.org>
2015-10-25 02:46:06 +0100

    Issue #2572475 by almaudoh, dpi: Remove global constants (SMS_GW_*, SMS_MSG_*)

---
+++ src/Tests/SmsFrameworkModuleInstallerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\sms\Tests\SmsFrameworkModuleInstallerTest.
+ */
+
+namespace Drupal\sms\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests the UI module installation of smsframework.
+ *
+ * @group SMS Frawework
+ */
+class SmsFrameworkModuleInstallerTest extends WebTestBase {
+
+
+  public function testUiInstallModule() {
+    $edit = [
+      'modules[SMS Framework][sms][enable]' => TRUE,
+    ];
+    $this->drupalLogin($this->rootUser);
+    $this->drupalPostForm('/admin/modules', $edit, 'Install');
+    $this->assertText('Module SMS Framework has been enabled.');
+  }
+
+}
