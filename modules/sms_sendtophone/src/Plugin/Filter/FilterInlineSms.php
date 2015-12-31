cb34d85fdf3201257136cd840e38b3be15745353
Aniebiet Udoh <almaudoh@yahoo.com>
2015-12-31 13:51:31 +0100

    Issue #2641886 by almaudoh: Port SMS Send To Phone to Drupal 8

---
+++ modules/sms_sendtophone/src/Plugin/Filter/FilterInlineSms.php
@@ -0,0 +1,153 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\sms_sendtophone\Plugin\Filter\FilterInlineSms.
+ */
+
+namespace Drupal\sms_sendtophone\Plugin\Filter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Markup;
+use Drupal\Core\Routing\LinkGeneratorTrait;
+use Drupal\Core\Url;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to align elements.
+ *
+ * @Filter(
+ *   id = "filter_inline_sms",
+ *   title = @Translation("Inline SMS"),
+ *   description = @Translation("Highlights text between <code>[sms][/sms]</code> tags and appends a 'send to phone' button."),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   settings = {
+ *     "display" = "icon",
+ *     "display_text" = @Translation("Send to phone"),
+ *     "default_icon" = 1,
+ *     "custom_icon_path" = ""
+ *   }
+ * )
+ */
+class FilterInlineSms extends FilterBase {
+
+  use LinkGeneratorTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $matches = array();
+    preg_match_all('/\[sms\](.*?)\[\/sms\]/i', $text, $matches, PREG_SET_ORDER);
+
+    $type = ($this->settings['display'] == 'icon') ? 'icon' : 'text';
+    foreach ($matches as $match) {
+      $text = str_replace($match[0], $this->theme($match[1], $type), $text);
+    }
+    return new FilterProcessResult($text);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    return $this->t('Text between [sms][/sms] tags will be highlighted and appended with a "send to phone" button.');
+  }
+
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $elements['display'] = array(
+      '#type' => 'radios',
+      '#title' => t('Show link as'),
+      '#description' => t('How to display the the "send to phone" link.'),
+      '#options' => array(
+        'text' => t('Text'),
+        'icon' => t('Icon'),
+      ),
+      '#default_value' => $this->settings['display'],
+    );
+
+    $elements['display_text'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Text for link'),
+      '#description' => t('If "Text" is selected above, the following text will be appended as a link.'),
+      '#size' => 32,
+      '#maxlength' => 32,
+      '#default_value' => $this->settings['display_text'],
+    );
+
+    $elements['default_icon'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Use default icon'),
+      '#description' => t('If "Icon" is selected above and this option is enabled, the default icon that came with the module will be used.'),
+      '#default_value' => $this->settings['default_icon'],
+    );
+
+    $elements['custom_icon_path'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Path to custom icon'),
+      '#description' => t('Provide a path to a custom icon. This icon will be used if "Icon" is selected above and the "Use default icon" option is disabled.'),
+      '#size' => 40,
+      '#maxlength' => 255,
+      '#default_value' => $this->settings['custom_icon_path'],
+      '#field_prefix' => Url::fromRoute('<none>', array(), array('absolute' => TRUE)),
+    );
+
+    return $elements;
+  }
+
+  /**
+   * Themes the message using a text link.
+   */
+  protected function theme($text, $type = 'icon') {
+    switch ($type) {
+      case 'text':
+        $markup = '(' . $this->settings['display_text'] . ')';
+        break;
+
+      case 'icon':
+      default:
+        if (!isset($this->settings["default_icon"]) || $this->settings["default_icon"] == 1) {
+          $icon_path = drupal_get_path('module', 'sms_sendtophone') . '/sms-send.gif';
+        }
+        else {
+          $icon_path = $this->settings["custom_icon_path"];
+        }
+
+        $title = $this->t('Send the highlighted text via SMS.');
+        $icon_path = base_path() . $icon_path;
+        // @todo: Figure out a better way to render the icon.
+        $markup = Markup::create("<img src='$icon_path' alt='{$this->settings["display_text"]}' title='$title'/>");
+        break;
+
+    }
+    $options = array(
+      'attributes' => array(
+        'title' => t('Send the highlighted text via SMS.'),
+        'class' => 'sms-sendtophone'
+      ),
+      'query' => array(
+        'text' => urlencode($text),
+        drupal_get_destination()
+      ),
+      'html' => TRUE,
+    );
+    $link = [
+      '#type' => 'link',
+      '#prefix' => '<span class="sms-sendtophone-inline">' . $text . '</span> ',
+      '#title' => $markup,
+      '#url' => Url::fromRoute('sms_sendtophone.page', ['type' => 'inline'], $options),
+    ];
+    return  $this->renderer()->renderPlain($link);
+  }
+
+  /**
+   * Encapsulates the renderer service for unit testing purposes.
+   *
+   * @return \Drupal\Core\Render\RendererInterface
+   */
+  protected function renderer() {
+    return \Drupal::service('renderer');
+  }
+
+}
