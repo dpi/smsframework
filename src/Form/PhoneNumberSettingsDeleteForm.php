cb1d43616169d59555b0f5c4f50f9c855ab6e803
dpi <pro@danielph.in>
2016-01-15 18:03:25 +0800

    Improved form layout improved strings changed redirect route after save

--- src/Form/PhoneNumberSettingsDeleteForm.php
+++ src/Form/PhoneNumberSettingsDeleteForm.php
@@ -12,7 +12,7 @@ use Drupal\Core\Url;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Form controller to delete phone number.
+ * Form controller to delete phone number settings.
  */
 class PhoneNumberSettingsDeleteForm extends EntityConfirmFormBase {
 
@@ -20,7 +20,7 @@ class PhoneNumberSettingsDeleteForm extends EntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getQuestion() {
-    return $this->t('Are you sure you want to delete SMS phone number %label?', array(
+    return $this->t('Are you sure you want to delete SMS phone number settings %label?', array(
       '%label' => $this->entity->label(),
     ));
   }
@@ -36,7 +36,7 @@ class PhoneNumberSettingsDeleteForm extends EntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getCancelUrl() {
-    return new Url('sms.gateway.list');
+    return new Url('sms.phone_number_settings.list');
   }
 
   /**
@@ -45,7 +45,7 @@ class PhoneNumberSettingsDeleteForm extends EntityConfirmFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->entity->delete();
 
-    drupal_set_message($this->t('Phone number %label was deleted.', array(
+    drupal_set_message($this->t('Phone number settings %label was deleted.', array(
       '%label' => $this->entity->label(),
     )));
 
