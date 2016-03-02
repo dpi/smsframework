20ea5eb7d79373d1affc487696a610f5dcd0d127
dpi <pro@danielph.in>
2016-03-03 01:04:24 +0800

    fixed sms blast working with entity phone numbers
    phone number provider no longer sends a message to every phone number on an entity
    createEntityWithPhoneNumber now correctly detects entity type depending on passed settings.

--- modules/sms_blast/src/Tests/SmsBlastWebTest.php
+++ modules/sms_blast/src/Tests/SmsBlastWebTest.php
@@ -8,6 +8,10 @@
 namespace Drupal\sms_blast\Tests;
 
 use Drupal\sms\Tests\SmsFrameworkWebTestBase;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\Component\Utility\Unicode;
+use Drupal\sms\Entity\PhoneNumberSettings;
 
 /**
  * Integration tests for the sms_blast module.
@@ -19,32 +23,63 @@ class SmsBlastWebTest extends SmsFrameworkWebTestBase {
   public static $modules = ['sms_user', 'sms_blast'];
 
   /**
-   * Tests sending sms blast.
+   * @var \Drupal\sms\Entity\PhoneNumberSettingsInterface
+   */
+  protected $phoneNumberSettings;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->drupalLogin($this->drupalCreateUser(['Send SMS Blast']));
+
+    $this->defaultSmsProvider->setDefaultGateway($this->testGateway);
+
+    $phone_field = FieldStorageConfig::create([
+      'entity_type' => 'user',
+      'field_name' => Unicode::strtolower($this->randomMachineName()),
+      'type' => 'telephone',
+    ]);
+    $phone_field->save();
+
+    FieldConfig::create([
+      'entity_type' => 'user',
+      'bundle' => 'user',
+      'field_name' => $phone_field->getName(),
+    ])->save();
+
+    $this->phoneNumberSettings = PhoneNumberSettings::create();
+    $this->phoneNumberSettings
+      ->setPhoneNumberEntityTypeId('user')
+      ->setPhoneNumberBundle('user')
+      ->setFieldName('phone_number', $phone_field->getName())
+      ->save();
+  }
+
+  /**
+   * Tests sending SMS blast.
    */
   function testSendBlast() {
-    $pre_count = count($this->getTestMessages());
+    // Create users with two phone numbers. Only one message should be sent to
+    // each user.
+    $phone_numbers = ['+123123123', '+456456456'];
+    for ($i = 0; $i < 3; $i++) {
+      // Create an unverified user.
+      $this->createEntityWithPhoneNumber($this->phoneNumberSettings, $phone_numbers);
+      // Create a verified user.
+      $entity = $this->createEntityWithPhoneNumber($this->phoneNumberSettings, $phone_numbers);
+      $this->verifyPhoneNumber($entity, $phone_numbers[0]);
+    }
+    $this->resetTestMessages();
 
-    // Set up test default gateway and test user.
-    $this->defaultSmsProvider->setDefaultGateway($this->testGateway);
-    $user = $this->drupalCreateUser(array('receive sms', 'Send SMS Blast'));
-    $this->drupalLogin($user);
-    $data = array(
-      'number' => '23415678900',
-      'status' => SMS_USER_CONFIRMED,
-      'code' => rand(1000, 9999),
-      'gateway' => '',
-    );
-    $user->sms_user = $data;
-    $user->save();
-
-    // Post sms blast to registered users.
-    $message = $this->randomString(140);
-    $this->drupalPostForm('sms_blast', array('message' => $message), t('Send'));
+    $edit['message'] = $this->randomString();
+    $this->drupalPostForm('sms_blast', $edit, t('Send'));
     $this->assertResponse(200);
-    $this->assertText('The message was sent to 1 users.', 'Message sent to 1 user.');
+    $this->assertText('Message sent to 3 users.');
 
     // Get the resulting message that was sent and confirm.
-    $this->assertTrue(count($this->getTestMessages()) > $pre_count, 'Successfully sent message');
+    $this->assertEqual(3, count($this->getTestMessages()), 'Sent three messages.');
   }
 
 }
