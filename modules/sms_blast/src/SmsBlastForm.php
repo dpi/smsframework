0a9ec5c824e68e453559ffa09b519bfa6b084511
dpi <pro@danielph.in>
2016-03-02 20:07:35 +0800

    Removed sms_user_get_number() and sms_user_send()

--- modules/sms_blast/src/SmsBlastForm.php
+++ modules/sms_blast/src/SmsBlastForm.php
@@ -9,6 +9,8 @@ namespace Drupal\sms_blast;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\sms\Message\SmsMessage;
+use Drupal\user\Entity\User;
 
 class SmsBlastForm extends FormBase {
 
@@ -42,6 +44,9 @@ class SmsBlastForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    /** @var \Drupal\sms\Provider\PhoneNumberProviderInterface $phone_number_provider */
+    $phone_number_provider = \Drupal::service('sms.phone_number');
+
     $result = db_select('sms_user', 'su')
       ->fields('su', array('uid'))
       ->condition('status', 2)
@@ -49,7 +54,9 @@ class SmsBlastForm extends FormBase {
 
     $count = 0;
     foreach ($result as $row) {
-      sms_user_send($row->uid, $form_state->getValue('message'));
+      $sms_message = new SmsMessage();
+      $sms_message->setMessage($form_state->getValue('message'));
+      $phone_number_provider->sendMessage(User::load($row->uid), $sms_message);
       $count++;
     }
     if ($count) {
