66e96d20638fde9941e992cb79cc3cea20900ff9
dpi <pro@danielph.in>
2016-04-21 00:22:50 +0800

    added some basic tests, accepting email validation failures.

---
+++ modules/sms_user/tests/src/Kernel/SmsFrameworkUserAccountRegistrationServiceTest.php
@@ -0,0 +1,221 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\sms_user\Kernel\SmsFrameworkUserAccountRegistrationServiceTest.
+ */
+
+namespace Drupal\Tests\sms_user\Kernel;
+
+use Drupal\Tests\sms\Kernel\SmsFrameworkKernelBase;
+use Drupal\sms\Entity\SmsMessage;
+use Drupal\sms\Entity\SmsMessageInterface;
+use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
+use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\Component\Utility\Unicode;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\sms\Entity\PhoneNumberSettings;
+
+/**
+ * Tests account registration.
+ *
+ * @group SMS Framework
+ * @coversDefaultClass \Drupal\sms_user\AccountRegistration
+ */
+class SmsFrameworkUserAccountRegistrationServiceTest extends SmsFrameworkKernelBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['system', 'sms', 'sms_user', 'sms_test_gateway', 'user', 'telephone', 'dynamic_entity_reference', 'field'];
+
+  /**
+   * @var \Drupal\sms_user\AccountRegistrationInterface
+   *
+   * The account registration service.
+   */
+  protected $accountRegistration;
+
+  /**
+   * @var \Drupal\sms\Provider\SmsProviderInterface
+   *
+   * The default SMS provider.
+   */
+  protected $smsProvider;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installSchema('system', ['sequences']);
+    $this->installConfig('sms_user');
+
+    $this->accountRegistration = $this->container->get('sms_user.account_registration');
+    $this->smsProvider = $this->container->get('sms_provider');
+
+    $this->gateway = $this->createMemoryGateway(['skip_queue' => TRUE]);
+    $this->smsProvider->setDefaultGateway($this->gateway);
+
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('sms');
+    $this->installEntitySchema('sms_phone_number_verification');
+
+    $this->phoneField = FieldStorageConfig::create([
+      'entity_type' => 'user',
+      'field_name' => Unicode::strtolower($this->randomMachineName()),
+      'type' => 'telephone',
+    ]);
+    $this->phoneField->save();
+
+    FieldConfig::create([
+      'entity_type' => 'user',
+      'bundle' => 'user',
+      'field_name' => $this->phoneField->getName(),
+    ])->save();
+
+    $this->phoneNumberSettings = PhoneNumberSettings::create();
+    $this->phoneNumberSettings
+      ->setPhoneNumberEntityTypeId('user')
+      ->setPhoneNumberBundle('user')
+      ->setFieldName('phone_number', $this->phoneField->getName())
+      ->setVerificationMessage($this->randomString())
+      ->save();
+  }
+
+  /**
+   * Ensure incoming SMS does not create messages or users.
+   */
+  public function testIncomingIgnored() {
+    $username = $this->randomMachineName();
+    $email = 'email@email.com';
+    $message = "E " . $email . "\nU " . $username;
+
+    $this->sendIncomingMessage('+123', $message);
+    $this->assertEquals(0, count($this->getTestMessages($this->gateway)), 'No messages were created');
+    $this->assertEquals(0, count(User::loadMultiple()), 'No users exist.');
+  }
+
+  /**
+   * Test user is created if a unrecognised phone number is used as sender.
+   */
+  public function testUnrecognisedCreateUser() {
+    $this->config('sms_user.settings')
+      ->set('account_registration.all_unknown_numbers.status', 1)
+      ->set('account_registration.all_unknown_numbers.reply.status', 1)
+      ->save();
+
+    $this->assertEquals(0, count(User::loadMultiple()), 'No users exist.');
+
+    $sender_number = '+123123123';
+    $this->sendIncomingMessage($sender_number, $this->randomString());
+
+    $this->assertEquals(1, count(User::loadMultiple()), 'One user created.');
+  }
+
+  /**
+   * Test a user is not created if the sender phone number is already used.
+   */
+  public function testUnrecognisedCreateUserPhoneNumberExists() {
+    $this->config('sms_user.settings')
+      ->set('account_registration.all_unknown_numbers.status', 1)
+      ->set('account_registration.all_unknown_numbers.reply.status', 1)
+      ->save();
+
+    $sender_number = '+123123123';
+    $this->createEntityWithPhoneNumber($this->phoneNumberSettings, [$sender_number]);
+    $this->resetTestMessages();
+
+    $this->assertEquals(1, count(User::loadMultiple()));
+
+
+    $this->sendIncomingMessage($sender_number, $this->randomString());
+    $this->assertEquals(1, count(User::loadMultiple()));
+    $this->assertEquals(0, count($this->getTestMessages($this->gateway)));
+  }
+
+  /**
+   * Test user is created from a preformatted message.
+   */
+  public function testPreformattedUserCreated() {
+    $this->config('sms_user.settings')
+      ->set('account_registration.formatted.status', 1)
+      ->set('account_registration.formatted.reply.status', 1)
+      ->save();
+
+    $username = $this->randomMachineName();
+    $email = 'email@email.com';
+    $message = "E " . $email . "\nU " . $username;
+    $this->sendIncomingMessage('+123', $message);
+
+    $user = user_load_by_name($username);
+    $this->assertTrue($user instanceof UserInterface, 'User was created');
+    $this->assertEquals($username, $user->label());
+    $this->assertEquals($email, $user->getEmail());
+  }
+
+  /**
+   * Test if a duplicated placeholder is confirmed.
+   */
+  public function testPreformattedPlaceholdersSuccess() {
+    $this->config('sms_user.settings')
+      ->set('account_registration.formatted.status', 1)
+      ->set('account_registration.formatted.reply.status', 1)
+      ->set('account_registration.formatted.incoming_messages.0', "[password] [username] [password]")
+      ->save();
+
+    $username = $this->randomMachineName();
+    $password = $this->randomMachineName();
+
+    $message = "$password $username $password";
+    $this->sendIncomingMessage('+123123123', $message);
+
+    $this->assertTrue(user_load_by_name($username) instanceof UserInterface, 'User was created');
+  }
+
+  /**
+   * Test if a duplicated placeholder is not confirmed
+   */
+  public function testPreformattedPlaceholdersFailure() {
+    $this->config('sms_user.settings')
+      ->set('account_registration.formatted.status', 1)
+      ->set('account_registration.formatted.reply.status', 1)
+      ->set('account_registration.formatted.incoming_messages.0', "[password] [username] [password]")
+      ->save();
+
+    $username = $this->randomMachineName();
+    $password = $this->randomMachineName();
+    $password2 = $this->randomMachineName();
+
+    $message = "$password $username $password2";
+    $this->sendIncomingMessage('+123123123', $message);
+
+    $this->assertFalse(user_load_by_name($username) instanceof UserInterface, 'User was not created');
+  }
+
+  /**
+   * Send an incoming SMS message.
+   *
+   * @param string $sender_number
+   *   The sender phone number.
+   * @param string $message
+   *   The message to send inwards.
+   */
+  protected function sendIncomingMessage($sender_number, $message) {
+    /** @var \Drupal\sms\Entity\SmsMessage $incoming */
+    $incoming = SmsMessage::create()
+      ->setSenderNumber($sender_number)
+      ->setDirection(SmsMessageInterface::DIRECTION_INCOMING)
+      ->setMessage($message)
+      ->addRecipients($this->randomPhoneNumbers(1));
+    $this->smsProvider->queue($incoming);
+  }
+
+//    $this->assertEquals([], sms_test_gateway_get_incoming('process'));
+//    $sms_message = $this->getLastTestMessage($this->gateway);
+//    $this->assertEquals('=', $sms_message->getMessage());
+
+}
