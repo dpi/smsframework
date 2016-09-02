ed1a36cc311a96b44cf3633c76a89b0a6c4893a0
dpi <pro@danielph.in>
2016-09-02 17:52:57 +0800

    Split status class into two.

--- src/Message/SmsMessageStatus.php
+++ src/Message/SmsMessageStatus.php
@@ -8,71 +8,8 @@ namespace Drupal\sms\Message;
 class SmsMessageStatus {
 
   /**
-   * Message was queued for sending.
-   */
-  const QUEUED = 'queued';
-
-  /**
-   * Message was successfully delivered to the recipient.
-   */
-  const DELIVERED = 'delivered';
-
-  /**
    * Message could not be processed due to an unknown problem with the gateway.
    */
   const ERROR = 'error';
-
-  /**
-   * Message could not be processed due to low credit.
-   */
-  const NO_CREDIT = 'no_credit';
-
-  /**
-   * Message expired and was not sent.
-   */
-  const EXPIRED = 'expired';
-
-  /**
-   * Message is pending delivery.
-   */
-  const PENDING = 'pending';
-
-  /**
-   * Message was rejected by the gateway.
-   */
-  const REJECTED = 'rejected';
-
-  /**
-   * Indicates a recipient of the message is invalid.
-   */
-  const INVALID_RECIPIENT = 'invalid_recipient';
-
-  /**
-   * Indicates the sender ID is invalid.
-   */
-  const INVALID_SENDER = 'invalid_sender';
-
-  /**
-   * Failed to authenticate with gateway.
-   */
-  const AUTHENTICATION = 'authentication';
-
-  /**
-   * Account error.
-   *
-   * Some configuration is required that can only be resolved on the gateway
-   * end.
-   */
-  const ACCOUNT_ERROR = 'account_error';
-
-  /**
-   * Too many requests.
-   */
-  const EXCESSIVE_REQUESTS = 'flooded';
-
-  /**
-   * Content of message invalid or not supported by gateway.
-   */
-  const CONTENT_INVALID = 'content_invalid';
 
 }
