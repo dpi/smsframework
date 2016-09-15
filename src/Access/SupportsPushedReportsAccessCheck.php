5c1814128c2a3cd30774113090f0c16353cdb478
dpi <pro@danielph.in>
2016-09-15 23:26:56 +0800

    Removed dead code

--- src/Access/SupportsPushedReportsAccessCheck.php
+++ src/Access/SupportsPushedReportsAccessCheck.php
@@ -3,12 +3,8 @@
 namespace Drupal\sms\Access;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Route;
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Routing\Access\AccessInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\sms\Entity\SmsGatewayInterface;
 
 /**
  * Checks if gateway supports pushed reports.
@@ -19,7 +15,6 @@ class SupportsPushedReportsAccessCheck implements AccessInterface {
    * Checks if the gateway supports pushed reports.
    */
   public function access(Request $request) {
-    // SmsGatewayInterface $sms_gateway) {
     if ($request->attributes->has('sms_gateway')) {
       /** @var \Drupal\sms\Entity\SmsGatewayInterface $sms_gateway */
       $sms_gateway = $request->attributes->get('sms_gateway');
