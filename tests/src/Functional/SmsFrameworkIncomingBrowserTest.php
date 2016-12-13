d8888711696177e4f7096332f9ff9532cd32f177
dpi <pro@danielph.in>
2016-12-13 13:48:26 +0800

    Removed some cruft from browser test, more assertions.

--- tests/src/Functional/SmsFrameworkIncomingBrowserTest.php
+++ tests/src/Functional/SmsFrameworkIncomingBrowserTest.php
@@ -12,13 +12,6 @@ use Drupal\Core\Url;
 class SmsFrameworkIncomingBrowserTest extends SmsFrameworkBrowserTestBase {
 
   /**
-   * The router builder.
-   *
-   * @var \Drupal\Core\Routing\RouteBuilderInterface
-   */
-  protected $routerBuilder;
-
-  /**
    * The HTTP client.
    *
    * @var \GuzzleHttp\Client
@@ -37,16 +30,18 @@ class SmsFrameworkIncomingBrowserTest extends SmsFrameworkBrowserTestBase {
    */
   protected function setUp() {
     parent::setUp();
-    $this->routerBuilder = $this->container->get('router.builder');
-    $this->httpClient = \Drupal::httpClient();//$this->container->get('http_client');
+    $this->httpClient = $this->container->get('http_client');
 
     $this->incomingGateway = $this->createMemoryGateway(['plugin' => 'incoming']);
     $this->incomingGateway
       ->setSkipQueue(TRUE)
-      ->save();;
-    $this->routerBuilder->rebuild();
+      ->save();
+    $this->container->get('router.builder')->rebuild();
   }
 
+  /**
+   * Test incoming route endpoint provided by 'incoming' gateway.
+   */
   public function testIncomingRouteEndpoint() {
     $messages[0] = [
       'message' => $this->randomString(),
@@ -59,23 +54,28 @@ class SmsFrameworkIncomingBrowserTest extends SmsFrameworkBrowserTestBase {
 
     $url = Url::fromRoute('sms.incoming.receive.' . $this->incomingGateway->id())
       ->setRouteParameter('sms_gateway', $this->incomingGateway->id())
-      ->setAbsolute();
+      ->setAbsolute()
+      ->toString();
 
     $options = [
       'json' => [
         'messages' => $messages,
       ],
-      'http_errors' => FALSE,
     ];
 
+    $this->assertTrue(TRUE, sprintf('POST request to %s', $url));
     $response = $this->httpClient
-      ->post($url->toString(), $options);
+      ->post($url, $options);
 
     $this->assertEquals(204, $response->getStatusCode(), 'HTTP code is 204');
     $this->assertEmpty((string) $response->getBody(), 'Response body is empty.');
 
     $incoming_messages = $this->getIncomingMessages($this->incomingGateway);
     $this->assertEquals(count($messages), count($incoming_messages), 'There are 2 messages');
+    foreach ($messages as $i => $message) {
+      $this->assertEquals($message['message'], $incoming_messages[$i]->getMessage(), "Message $i contents are same.");
+      $this->assertEquals($message['recipients'], $incoming_messages[$i]->getRecipients(), "Message $i recipients are same.");
+    }
   }
 
 }
