cefce066cc4f1aec4e393a6142c5d0a6be78bb60
Aniebiet Udoh <almaudoh@yahoo.com>
2017-08-21 10:50:17 +0100

    Issue #2857911 by almaudoh: Use setConfiguration() to merge default configuration values

--- src/Plugin/SmsGatewayPluginCollection.php
+++ src/Plugin/SmsGatewayPluginCollection.php
@@ -2,10 +2,29 @@
 
 namespace Drupal\sms\Plugin;
 
+use Drupal\Component\Plugin\PluginManagerInterface;
 use Drupal\Core\Plugin\DefaultSingleLazyPluginCollection;
 
 /**
  * Provides a container for lazily loading SMS Gateway plugins.
  */
 class SmsGatewayPluginCollection extends DefaultSingleLazyPluginCollection {
+
+  /**
+   * Constructs a new SmsGatewayPluginCollection object.
+   *
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $manager
+   *   The manager to be used for instantiating plugins.
+   * @param string $instance_id
+   *   The ID of the plugin instance.
+   * @param array $configuration
+   *   An array of configuration.
+   */
+  public function __construct(PluginManagerInterface $manager, $instance_id, array $configuration) {
+    $this->manager = $manager;
+    if ($instance_id !== NULL) {
+      $this->addInstanceId($instance_id, $configuration);
+    }
+  }
+
 }
